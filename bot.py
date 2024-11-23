15dc2e3b679c39c5a8dac578a2f1d42b1f1ac3ff
Jan2013rd <jan.p.staszewski@gmail.com>
2024-11-23 14:50:44 +0000

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,31 @@
+
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Zalogowaliśmy się jako {bot.user}')
+
+@bot.command()
+async def cool(ctx):
+    """Says if a user is cool.
+
+    In reality this just checks if a subcommand is being invoked.
+    """
+    if ctx.invoked_subcommand is None:
+        await ctx.send(f'No, {ctx.subcommand_passed} is not cool')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Cześć, jestem bot{bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+bot.run("MTMwNzM2MTA4MjQwNjM0MjY5OA.GbQvJz.dn6EKJoUodlJJ2fYMWwdftwb-r0sys808H6Unc")
